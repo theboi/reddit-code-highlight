67cc0de13786371f4e1540b159f0e2f532ab32c8
Ryan The <ryan.the.2006@gmail.com>
2020-07-24 23:53:07 +0800

    ✨ Add features object with Map to contain keywords

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-console.log(document.querySelectorAll("code"))
-for (element of document.querySelectorAll("code")) {
-  element.innerHTML = ""
+// Wait for Reddit to finish loading before changing styles
+setTimeout(() => {
+  for (element of document.querySelectorAll("pre > code")) {
+    let theme = window.getComputedStyle(element).getPropertyValue('--background')
+    console.log(theme)
+    const content = element.innerHTML;
+    element.innerHTML = `<span class="rch-ext-method">${content}</span>`
+  }
+}, 2000)
+
+const highlightCode = (string, lang) => {
+  string.split(/\s/).map().join(" ")
 }
+
+const features = {
+  javascript: new Map(Object.entries({
+    var: "#fff",
+    let: "#fff",
+    const: "",
+    class: "",
+  }))
+}
+
+//TODO: Go thru each word in string, check Hashmap if word is keyword, if yes, highlight according to color
