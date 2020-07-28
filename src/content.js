d0e345f7ea26d484c3dd070730e704818bb36d58
Ryan The <ryan.the.2006@gmail.com>
2020-07-28 20:09:47 +0800

    💩 Remove /dist

--- src/content.js
+++ src/content.js
@@ -1,7 +1,19 @@
 'use strict';
 
+const OPTIONS_AVAILABLE = ["light-style", "dark-style"]
+
 const script = document.createElement('script');
 script.setAttribute("type", "module");
 script.setAttribute("src", chrome.extension.getURL('./dist/main.js'));
 const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
 head.insertBefore(script, head.lastChild);
+
+try {
+  chrome.storage.sync.get(OPTIONS_AVAILABLE, (result) => {
+    for (const option in result) {
+      window.sessionStorage.setItem(option, result)
+    }
+  })
+} catch (e) {
+  console.error("ERROR: ", e)
+}
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,14 @@ module.exports = {
         test: /\.css$/, use: [
           'style-loader', 'css-loader'
         ]
+      },
+      {
+        test: /\.js$/, exclude: /node_modules/, use: {
+          loader: "babel-loader", options: {
+            plugins: ['@babel/plugin-syntax-dynamic-import']
+          }
+        }
       }
     ]
-  }
+  },
 }
