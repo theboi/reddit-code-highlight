2dbcadb8a89b83c899919447bae09b56814f6ddf
Ryan The <ryan.the.2006@gmail.com>
2020-07-28 19:59:35 +0800

    🐛 Fix HTML entities not rendered properly

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github.css';
+const OPTIONS_AVAILABLE = ["light-style", "dark-style"]
+let isDarkMode = false;
 
 setTimeout(() => {
-  for (const element of document.querySelectorAll("pre > code")) {
-    element.innerHTML = hljs.highlightAuto(element.innerHTML).value
+  for (const element of document.querySelectorAll("pre code")) {
+    hljs.highlightBlock(element)
   }
+  window.html
+  OPTIONS_AVAILABLE.forEach((option) => { })
 }, 2000)
+
+// console.log(window.sessionStorage.getItem(option))
